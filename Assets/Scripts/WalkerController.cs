dd7c8e4d269c52eb9e07bdc39672b129c7252f83
自動車班 <driving.ability@gmail.com>
2023-06-22 20:27:56 +0900

    振り返るanimation

--- Assets/Scripts/WalkerController.cs
+++ Assets/Scripts/WalkerController.cs
@@ -5,9 +5,19 @@ using UnityEngine.AI;
 
 public class WalkerController : MonoBehaviour
 {
-    [SerializeField] private Transform trs;
-    private CarController _player;
-    private Vector2 _smoothDeltaPosition = Vector2.zero;
+    [SerializeField] private Transform _head;
+    [SerializeField] private float _lookAtBoarder = 50.0f;
+    [SerializeField] private Transform _lookAtTargetTransform;
+    [SerializeField] Vector3 _lookAtTargetPosition;
+    private float _weightRatio = 2.5f;
+    float _lookAtCoolTime = 0.2f;
+    public float _lookAtHeatTime = 0.2f;
+    public bool _looking = true;
+    private bool _looked = false;
+    private float _lookAtWeight = 0.0f;
+
+    private Vector3 _lookAtPosition;
+    private CarController _myCar;
     private Vector2 _velocity = Vector2.zero;
     private Vector3 _targetPos;
     private NavMeshAgent _agent;
@@ -15,19 +25,25 @@ public class WalkerController : MonoBehaviour
     int _animIDVelocityX;
     int _animIDVelocityY;
     bool _hasAnimator;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        //_player = FindAnyObjectByType<CarController>();
         _agent = GetComponent<NavMeshAgent>();
-
+        _myCar = FindObjectOfType<CarController>();
         _targetPos = transform.position + 130 * Vector3.forward;
-        _targetPos = trs.position;
+
         _agent.destination = _targetPos;
         _agent.updatePosition = false;
-
         _hasAnimator = TryGetComponent<Animator>(out _animator);
         AssignAnimationIDs();
+
+        if (_head)
+        {
+            _lookAtTargetPosition = _head.position + transform.position;
+            _lookAtPosition = _lookAtTargetPosition;
+        }
     }
     private void AssignAnimationIDs()
     {
@@ -56,6 +72,7 @@ public class WalkerController : MonoBehaviour
             _animator.SetFloat(_animIDVelocityX, _velocity.x);
             _animator.SetFloat(_animIDVelocityY, _velocity.y);
         }
+
         transform.position = _agent.nextPosition;
     }
     void OnAnimatorMove()
@@ -77,8 +94,26 @@ public class WalkerController : MonoBehaviour
             }
         }
     }
-    private void OnFootstep()
+    private void OnAnimatorIK()
     {
-        return;//Error解消
+        _lookAtTargetPosition = _lookAtTargetTransform.position;
+        _lookAtTargetPosition.y = _head.position.y;
+        int lerpTarget;
+        if (!_looked)
+        {
+            lerpTarget = 1;
+        }
+        else
+        {
+            lerpTarget = 0;
+        }
+        Debug.Log(_lookAtWeight);
+        _lookAtWeight = Mathf.Lerp(_lookAtWeight, lerpTarget, Time.deltaTime * _weightRatio);
+        _animator.SetLookAtWeight(_lookAtWeight, 0.2f, 0.5f, 0.7f, 0.2f);
+        _animator.SetLookAtPosition(_lookAtTargetPosition);
+        if (_lookAtWeight / lerpTarget > 0.9999f)
+        {
+            _looked = true;
+        }
     }
 }
