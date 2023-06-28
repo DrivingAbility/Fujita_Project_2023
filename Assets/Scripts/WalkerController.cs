8c65225a3b6bce76e883c2da7844c20910186118
RyotaroFujita <ryoufuji1999@gmail.com>
2023-06-28 14:00:09 +0900

    スクリプト修正

--- Assets/Scripts/WalkerController.cs
+++ Assets/Scripts/WalkerController.cs
@@ -7,12 +7,14 @@ using UnityEngine.AI;
 public class LookAtWeight
 {
     [SerializeField] private float _maxWeight = 0.95f;
+    [SerializeField] private float _minWeight = 0.05f;
     [SerializeField] private float _bodyWeight = 0.05f;
     [SerializeField] private float _headWeight = 0.5f;
     [SerializeField] private float _eyeWeight = 0.8f;
     [SerializeField] private float _clampWeight = 0.05f;
 
     public float MaxWeight { get => _maxWeight; }
+    public float MinWeight { get => _minWeight; }
     public float BodyWeight { get => _bodyWeight; }
     public float HeadWeight { get => _headWeight; }
     public float EyeWeight { get => _eyeWeight; }
@@ -22,9 +24,11 @@ public class WalkerController : MonoBehaviour
 {
     [SerializeField] private LookAtWeight _weightParams;
     [SerializeField] private float _lookAtBoarder = 10;
+    [SerializeField] private float _lookAtTargetHeight = 1.0f;
     [SerializeField] private float _weightRatio = 5.0f;
     [SerializeField] private float _avoidanceAngle = 45;
     private Vector3 _lookAtTargetPosition;
+    private bool _isWeightOverMax = false;
     private bool _looked = false;
     private float _lookAtWeight = 0.0f;
     private CarController _myCar;
@@ -77,7 +81,6 @@ public class WalkerController : MonoBehaviour
         float dx = Vector3.Dot(transform.right, worldDeltaPosition);
         float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
         Vector2 deltaPosition = new Vector2(dx, dy);
-
         // velocity (速度) を更新します
         _velocity = deltaPosition / Time.deltaTime;
         if (_hasAnimator)
@@ -105,18 +108,25 @@ public class WalkerController : MonoBehaviour
     }
     private void OnAnimatorIK()
     {
+        LookAtIK();
+    }
+    private void LookAtIK()
+    {
         if (!_myCar) return;
-        _lookAtTargetPosition = _myCar.transform.position;
+        _lookAtTargetPosition = _myCar.transform.position + _lookAtTargetHeight * Vector3.up;
         var isNear = Vector3.Distance(_lookAtTargetPosition, transform.position) < _lookAtBoarder;
-        var lerpTarget = (!_looked && isNear) ? 1 : 0;
+        var lerpTarget = (!_isWeightOverMax && isNear) ? 1 : 0;
         _lookAtWeight = Mathf.Lerp(_lookAtWeight, lerpTarget, Time.deltaTime * _weightRatio);
         _animator.SetLookAtWeight(_lookAtWeight, _weightParams.BodyWeight, _weightParams.HeadWeight,
             _weightParams.EyeWeight, _weightParams.ClampWeight);
         _animator.SetLookAtPosition(_lookAtTargetPosition);
         if (!_looked && _lookAtWeight > _weightParams.MaxWeight)
         {
+            _isWeightOverMax = true;
+        }
+        if (_isWeightOverMax && _lookAtWeight < _weightParams.MinWeight)
+        {
             _looked = true;
-            Debug.Log(gameObject.name + " Looked Player!!");
         }
     }
 }
