a81ad3fe5e77cb6dcd5be47bd97b17135ba2e101
自動車班 <driving.ability@gmail.com>
2023-10-12 14:52:14 +0900

    修正

--- Assets/Scripts/RandomObjInstanciate.cs
+++ Assets/Scripts/RandomObjInstanciate.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class RandomObjInstanciate : MonoBehaviour
 {
-    [SerializeField] GameObject dotObj;
-    [SerializeField] int _numPerSquareMeter = 10;
+    [SerializeField] GameObject _dotObj;
+    [SerializeField] float _numPerSquareMeter = 10;
     [SerializeField, Range(0f, 0.1f)] float _minDotSize = 0.01f;
     [SerializeField, Range(0f, 0.1f)] float _maxDotSize = 0.03f;
-    [SerializeField] Vector2 _meshSquareLength = new Vector2(10, 10);
-    [SerializeField] Vector2 _tilling = Vector2.one;
-    [SerializeField, Range(0f, 2f)] float height;
-    string parentPlaneName = "ParentDotPlane";
+    [SerializeField] Vector2 _meshSquareLength = new Vector2(10.0f, 10.0f);
+    [SerializeField] Vector2 _tilling = new Vector2(1.0f,1.0f);
+    [SerializeField, Range(0f, 2f)] float _height;
+    string _parentPlaneName = "ParentDotPlane";
     [ContextMenu("GenerateDots")]
     public void GenerateDots()
     {
@@ -19,22 +19,22 @@ public class RandomObjInstanciate : MonoBehaviour
     }
     public void GenerateDotsMesh()
     {
-        if (GameObject.Find(parentPlaneName)) DestroyImmediate(GameObject.Find(parentPlaneName));
-        var parentObj = new GameObject(parentPlaneName);
+        if (GameObject.Find(_parentPlaneName)) DestroyImmediate(GameObject.Find(_parentPlaneName));
+        var parentObj = new GameObject(_parentPlaneName);
 
-        int dotsCount = (int)(_meshSquareLength.x * _meshSquareLength.y) * _numPerSquareMeter;
+        int dotsCount = (int)(_meshSquareLength.x * _meshSquareLength.y * _numPerSquareMeter);
         CombineInstance[] combine = new CombineInstance[dotsCount];
         for (int i = 0; i < dotsCount; i++)
         {
             Vector3 position = new Vector3(0, 0, 0);
             position.x = Random.Range(-_meshSquareLength.x / 2, _meshSquareLength.x / 2);
-            position.y = Random.Range(0, height);
+            position.y = Random.Range(0, _height);
             position.z = Random.Range(-_meshSquareLength.y / 2, _meshSquareLength.y / 2);
 
-            dotObj.transform.position = position;
-            dotObj.transform.localScale = Vector3.one * Random.Range(_minDotSize, _maxDotSize);
-            combine[i].mesh = dotObj.GetComponent<MeshFilter>().sharedMesh;
-            combine[i].transform = dotObj.GetComponent<MeshFilter>().transform.localToWorldMatrix;
+            _dotObj.transform.position = position;
+            _dotObj.transform.localScale = Vector3.one * Random.Range(_minDotSize, _maxDotSize);
+            combine[i].mesh = _dotObj.GetComponent<MeshFilter>().sharedMesh;
+            combine[i].transform = _dotObj.GetComponent<MeshFilter>().transform.localToWorldMatrix;
         }
 
         var combineObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
@@ -46,7 +46,7 @@ public class RandomObjInstanciate : MonoBehaviour
         parentMeshFilter.sharedMesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
         parentMeshFilter.sharedMesh.name = "CombinedMesh";
         parentMeshFilter.sharedMesh.CombineMeshes(combine);
-        parentMeshRenderer.sharedMaterial = dotObj.GetComponent<MeshRenderer>().sharedMaterial;
+        parentMeshRenderer.sharedMaterial = _dotObj.GetComponent<MeshRenderer>().sharedMaterial;
         parentMeshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
 
         for (int i = 0; i < _tilling.x; i++)
